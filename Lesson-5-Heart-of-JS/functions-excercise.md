e6d016aef396faf510ab09d24be6e01625fa6f46
Ralphkay <114482210+raamponsah@users.noreply.github.com>
2023-01-23 15:44:40 +0000

    Updated

---
+++ Lesson-5-Heart-of-JS/functions-excercise.md
@@ -0,0 +1,40 @@
+# Functions Excercises
+
+Write a function that accept firstname, surname and age. The function should determine whether the person is above 13 or below.
+If the person is below 13 display this "You are not allowed, Kofi" where Kofi is the firstname
+
+```js 
+
+function checkUserAge(firstname, surname, age) {
+    if(age < 13){
+        console.log(`You are not allowed, ${firstname}`)
+    }
+}
+
+checkUserAge('Kofi','Amoah, 13)
+```
+
+
+Write a function that accepts a message and 
+returns the length of the message.
+
+```js 
+// let items = ['pocket'];
+// items.length
+// String are array like
+function checkMessageLength(message) {
+    return message.length
+}
+```
+
+# Assignment
+
+1. Write a function that accepts a message and check whether  it more than 200. If it is more than 200 characters,  the function should return "You have exceeded 200 characters, be smart"
+
+2. Write a function that accepts a an array of numbers and returns the the sum of the array elements.
+
+3. Write a function that accepts two arrays and display them in the console.
+
+4. Write  a function that accepts three numbers and returns the minimum and maximum values. _[Hint use only if and else statements]_
+
+5. Write a function that accepts an array of numbers and returns a new array of odd numbers only.
--- Lesson-5-Heart-of-JS/functions.js
+++ Lesson-5-Heart-of-JS/functions.js
@@ -1,72 +1,139 @@
-// // Function Barebones
-// // function messageMe(){
-// //     console.log("Hi, what is your name?")
-// // }
+// Funtions
 
-// // call it or evoke the function
-// // messageMe()
+/**
+ * Write a function that accepts two numbers and return
+ * the sum of the two numbers.
+ */
 
-// // function visitThisLink(){
-// //     console.log("http://localhost:2000/homepage")
-// // }
+// function add(a,b){
+//     return a+b;
 
-// // visitThisLink()
-
-// // functions have arguments
-// // function messageMe(nameOfPerson){
-// //     console.log(`Hi, ${nameOfPerson}`)
-// // }
-
-// // function visitThisLink(link){
-// //     console.log(link)
-// // }
-
-// // messageMe("Delphine Mawuli")
-// // visitThisLink("https://google.com")
-
-// function messageEmployees(employees){
-//     for(let employee of employees){
-//         console.log(`Hello, ${employee}`)
-//     }
 // }
-// let persons =["John","Ama","Karikari","Yaw"]
-// messageEmployees(persons)  
 
-// console.log("================================================================")
+// console.log(add(25,32))
 
-// let students = ["Kofi","Ama","Quincy","Gloria"]
+/**
+ * Write a simple function that accepts two numbers
+ * that returns subtraction of the two.
+ * Part B
+ * Ensure that the return value is always a positive value.
+ */
 
-// messageEmployees(students)
-
-
-// let iceCreams = ["Choco","Vanilla","Biscuits"]
-
-// messageEmployees(iceCreams)
-
-
-
-
-// function sendEmployee(name, email){
-//     if(email == "mawuli@made.com"){
-//         console.log("You are not allowed to send this email");
-//     }else{
-
-//         console.log(`Hello ${name} please is this your email: ${email}`)
+// function subtract(a,b){
+//     if(a<b){
+//         return b-a;
 //     }
+//     if(b<a){
+//         return a-b;
+//     }
+
 // }
 
 
-// sendEmployee("Delphine","mawuli@delphine.com")
+// console.log(subtract(3,2))
 
+/**
+ * Write a function that accepts three numbers and returns
+ * the sum of them
+ */
 
-// function checkAgeLimit(age){
-
-//     if(age>=60){
-//         console.log("Retirement package must be sent!")
-//     }else{
-//         console.log("Person is still working")
-//     }
+// function sum(a,b,c){
+//     return a+b+c;
 // }
 
-// checkAgeLimit(60)
+// console.log(sum(1,2,3))
+
+
+
+/**
+ * Write a function that accepts the firstName and the Surname
+ * and returns the fullname with a space
+ *
+ */
+
+// function fullName(firstName, surname){
+//     // return firstName +' '+ surname;
+//     return `${firstName} ${surname}`;
+// } 
+
+
+// console.log(fullName("Henry","Quartey"))
+
+
+
+
+
+/**
+ * write a function that accept firstname, surname and age.
+ * The function should determine whether the person is 
+ * above 13 or below.
+ * If the person is below 13 display this "You are not allowed, Kofi"
+ * where Kofi is the first name
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Write a function that accepts a username, 
+ * the user's age and the users location and 
+ * returns the information when consoled
+ */
+
+
+
+// function returnUserInfo(username, age, location){
+//     return `The user ${username} is ${age} years old and lives in ${location}`
+// }
+
+
+// let userInfo = returnUserInfo("Ben", 23, "Accra") // The user Ben is 23 years old and lives in Accra
+
+// console.log(userInfo)
+
+
+
+
+/**
+ * Write a function that accepts an array and multiplies 
+ * each by 10 and returns a new array
+ */
+
+
+// function multiplesOfTen(arrayOfNumbers){
+//     let m10Array = []
+//     for(i=0;i<arrayOfNumbers.length;i++){
+//         let item = arrayOfNumbers[i]*10
+//         m10Array.push(item)
+//     }
+//     return m10Array;
+// }
+
+
+// multiplesOfTen([1,2,3,4])
+
+/**
+ * Write a function that checks whether a value is 
+ * an integer or not and returns true or false;
+ */
+
+
+
+
 

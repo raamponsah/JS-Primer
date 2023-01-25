7347f7694bc26635be2e6fece9658b0b11013b99
Ralphkay <114482210+raamponsah@users.noreply.github.com>
2023-01-25 15:20:37 +0000

    updated

---
+++ Lesson-9-DOM/creatingDom.js
@@ -0,0 +1,5 @@
+let body = document.getElementsByTagName('body')[0]
+let h1 = document.createElement('h1');
+
+h1.innerText = "Am coming from JavaScript"
+body.appendChild(h1);
--- Lesson-9-DOM/dom.js
+++ Lesson-9-DOM/dom.js
@@ -1,10 +1,70 @@
-let h1Elements = document.getElementsByTagName("h1")
+let html = document.getElementById('html');
+let body = document.getElementById('body');
+let h1Elements = document.getElementsByTagName("p")
+let firstElement = document.getElementById('first')
+let pFirst = document.getElementById('firstPara')
 
-for (var i = 0; i < h1Elements.length; i++) {
-console.log(h1Elements[i]);
-h1Elements[i].style.backgroundColor = "Yellow";
+let pElements = document.getElementsByTagName('p')
 
+firstElement.style.color = 'yellow'
+firstElement.style.background ='black'
+
+
+
+
+
+function applyStyles(){
+    for(let i =0; i < pElements.length; i++){
+       setTimeout(function(){
+        pElements[i].style.background = `rgb(${i*55},${i*44},${i*90})`
+        pElements[i].style.color = 'white'
+        pElements[i].style.borderRadius = '10px'
+        pElements[i].style.width = '200px'
+        pElements[i].style.padding = '10px'
+        pElements[i].style.color = 'pink'
+        pElements[i].style.border = '5px solid orange'
+       }, `${i*2000}`)
+    }
 }
 
 
-console.log(h1Elements.length)
+applyStyles()
+
+
+
+
+
+
+
+
+
+
+
+
+body.style.background = 'pink'
+body.style.display = 'flex'
+body.style.justifyContent = 'center'
+body.style.flexDirection = 'column'
+body.style.width = '900px'
+body.style.margin = '0 auto'
+body.style.padding = '20px'
+
+
+html.style.background = 'white'
+
+
+
+
+
+
+
+// for (var i = 0; i < h1Elements.length; i++) {
+// console.log(h1Elements[i]);
+// h1Elements[i].style['backgroundColor'] = "pink";
+
+// console.log(h1Elements[i].style['backgroundColor'])
+
+// }
+
+
+console.log(firstElement.style)

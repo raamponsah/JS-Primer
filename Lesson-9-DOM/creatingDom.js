2601e222bae54bf215f4d36285d9c9748838ade5
Ralphkay <114482210+raamponsah@users.noreply.github.com>
2023-01-25 15:26:25 +0000

    updated

--- Lesson-9-DOM/creatingDom.js
+++ Lesson-9-DOM/creatingDom.js
@@ -1,5 +1,9 @@
 let body = document.getElementsByTagName('body')[0]
 let h1 = document.createElement('h1');
+let p = document.createElement('p');
 
 h1.innerText = "Am coming from JavaScript"
+p.innerText ='I am a young paragraph in JavaScript'
+
 body.appendChild(h1);
+body.appendChild(p);

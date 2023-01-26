880f54ef4dec0b5cd9ff23ea40e84fad6f84a025
Ralph <ramponsah026@st.ug.edu.gh>
2023-01-26 14:09:30 +0000

    updates

--- Lesson-9-DOM/DOM-2/main.js
+++ Lesson-9-DOM/DOM-2/main.js
@@ -1,3 +1,32 @@
-const body = document.getElementsByTagName('body')[0]
-const h1 = document.createElement('h1');
+let p = document.querySelector('p.brown a');
+p.innerHTML ="<strike>Link</strike>"
+
+
+// let h = document.querySelector('div#house ')
+
+function smartSelector(s){
+    let x =s.split(" ")
+    let y =x[x.length-1]
+    let z = document.createElement(y)
+    let d =applyStyles(z)
+    display(d)
+
+}
+
+function applyStyles(d){
+    d.style.color = "red"
+    d.style.fontSize = "20px"
+    d.style.fontWeight = "bold"
+    d.style.fontStyle = "italic"
+    d.style.textDecoration = "underline"
+    return d;
+}
+
+
+function display(d){
+    console.log(d)
+}
+
+console.log(smartSelector(`ul li h2`))
+
 
---
+++ Lesson-9-DOM/Events/events.js
@@ -0,0 +1,73 @@
+
+
+
+// Recommended way
+// addEventListener('mouseout', function(e){
+//     console.log(e)
+// })
+
+// addEventListener('click', function(e){
+//     console.log(e)
+// })
+
+// addEventListener('keyup', function(e){
+//     console.log(e)
+// })
+
+
+// addEventListener('keydown', function(e){
+//     console.log(e)
+// })
+
+
+// addEventListener('keypress', function(e){
+//     console.log(e)
+// })
+
+
+// addEventListener('dblclick', function(e){
+//     console.log(e)
+// })
+
+
+
+// addEventListener('resize', function(e){
+//     console.log(e)
+// })
+
+
+// For all the html elements you can apply or listen to an event
+// on them
+
+// let h1 = document.querySelector('h1');
+// let p = document.querySelector('p')
+
+// h1.addEventListener('click',function(e){
+//     console.log('You\'ve clicked me')
+// })
+
+
+// p.addEventListener('dblclick',function(){
+//     alert("Hello P")
+// })
+
+
+
+// let b = document.createElement('button')
+
+// let body = document.getElementsByTagName('body')[0]
+
+// b.innerText = 'Click Me'
+
+// b.addEventListener('click',function(){
+//     console.log('Am a button!')
+// })
+
+// body.appendChild(b)
+
+
+
+// Create an array of elements of buttons (5)
+// Attach event listeners to each
+// When clicked, the button to console log 'I am at position:' its index
+// all in js
